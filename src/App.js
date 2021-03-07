14b0ffc61b5dfc2c6780a153b6694958657352cd
Jonatan Sánchez <jona2255@gmail.com>
2021-03-07 10:39:36 +0100

    Cambiado el favicon. el título y la web en general

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import gatito from "./gatito-meme.png"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <img src={gatito} alt="Gatito cansadito de programar" />
+      <p>Representación gráfica de un programador arreglando bugs, pero con un gatito, que siempre es más bonito</p>
     </div>
   );
 }
